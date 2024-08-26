3542da13a5bbd282203645b2b172553830da6d28
zabe <jay@zabe.dev>
2024-08-26 19:03:18 +0800

    fix responsive sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Home() {
 	return (
 		<Container className="space-x-4">
-			<Skeleton className="h-20 w-[50%]" />
-			<Skeleton className="h-20 w-[50%]" />
+			<div className="flex w-full flex-col items-center space-y-4 lg:flex-row lg:space-x-2 lg:space-y-0">
+				<Skeleton className="h-20 w-[80%] lg:w-[50%]" />
+				<Skeleton className="h-20 w-[80%] lg:w-[50%]" />
+			</div>
 		</Container>
 	);
 }
